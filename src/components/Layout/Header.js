be34083d6b9c80f06dec4ee03b098422cac115b0
Amir <amir.ukic123@gmail.com>
2022-12-22 17:27:29 +0100

    context-update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,19 @@
 import { Fragment } from "react";
-import mealsImage from "../../assets/meals.jpg"
-import classes from "./Header.module.css"
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>Meals</h1>
-            <HeaderCartButton />
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="jela"/>
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="jela" />
+      </div>
     </Fragment>
-}
+  );
+};
 
-export default Header
+export default Header;
