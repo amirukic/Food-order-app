4613226405f952c1ac84d93a601e664a2cb33664
Amir <amir.ukic123@gmail.com>
2023-04-14 02:38:37 +0200

    kjkszpj

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -40,7 +40,7 @@ const AvailableMeals = () => {
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
-        <p>Loading...</p>
+        <p>Loading....</p>
       </section>
     );
   }
